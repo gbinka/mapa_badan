8b5d00fd2e3e5735e0f92591fafbf620e15d5469
Kazik <g.binka@gmail.com>
2018-11-28 22:33:14 +0100

    Working dropdown

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,77 @@
 <template>
-    <div class="container-fluid full-width">
-        <div class="row row-no-gutter">
-            <div class="col-1"></div>
-            <div id="logos" class="col-11 d-flex align-items-center">
-                <a class="navbar-brand" href="#"><img src="../assets/ieiak_logo.png" />  </a>
-                <a class="navbar-brand" href="#"><img src="../assets/uw_logo.jpg" />  </a>
-            </div>
-        </div>
-
-        <div class="row row-no-gutter">
-            <div class="col-12">
-                <nav class="navbar p-0 navbar-expand-lg navbar-dark col"> 
-    
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                            <span class="navbar-toggler-icon"></span>
-                                            </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav mx-auto nav-fill text-uppercase main-menu ">
-                            <li class="nav-item"><a class="nav-link" href="#">Instytut</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">Dla studentów</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">Dla kandydatów</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">Wokół etnologii</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">Biblioteka i archiwum</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">En</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#">Mapa</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    
-        <div class='row row-no-gutter tags-menu'>
-            <div class="col-1"></div>
-            <div class="col-6">
-                <TagBrowser :tags="tags" :width="'100%'" @tagSelected="tagSelected($event)"></TagBrowser>
-            </div>
-            <div class="col-5"> 
-              <div class="dropdown">
-                  <button class="dropbtn" @click="researchType =! researchType">Wybierz rodzaj badań</button>
-                    <div id="myDropdown" class="dropdown-content" v-show="researchType">
-                      <a href="#home">Studenckie</a>
-                      <a href="#about">Doktoranckie</a>
-                      <a href="#contact">Pracownicze</a>
-                    </div>
-                  </div>
-            </div>
-        </div>
-    
+  <div>
+    <div class="row row-no-gutter">
+      <div class="col-1"></div>
+      <div id="logos" class="col-11 d-flex align-items-center">
+        <a class="navbar-brand" href="#">
+          <img src="../assets/ieiak_logo.png">
+        </a>
+        <a class="navbar-brand" href="#">
+          <img src="../assets/uw_logo.jpg">
+        </a>
+      </div>
     </div>
+
+    <div class="row row-no-gutter">
+      <div class="col-12">
+        <nav class="navbar p-0 navbar-expand-lg navbar-dark col">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto nav-fill text-uppercase main-menu">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Instytut</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Dla studentów</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Dla kandydatów</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Wokół etnologii</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Biblioteka i archiwum</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">En</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mapa</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <div class="row row-no-gutter tags-menu">
+      <div class="col-1"></div>
+      <div class="col-6">
+        <TagBrowser :tags="tags" :width="'100%'" @tagSelected="tagSelected($event)"></TagBrowser>
+      </div>
+      <div class="col-5">
+        <div class="dropdown">
+          <button class="dropbtn" @click="researchType =! researchType">Wybierz rodzaj badań</button>
+          <div id="myDropdown" class="dropdown-content" v-show="researchType">
+            <a href="#home">Studenckie</a>
+            <a href="#about">Doktoranckie</a>
+            <a href="#contact">Pracownicze</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,18 +1,308 @@
 <template>
-    <div class="row row-no-gutter d-flex flex-fill">
-        <div class="col-lg-1 col-2 left-menu flex-fill d-flex">
-          <span style="width:100%">ENG</span>
-                
-            </div>
-        <div class="col-lg-11 col-10">MAPA</div>
+  <div class="row row-no-gutter flex-fill">
+    <div class="col-lg-1 col-2 left-menu flex-column">
+      <div class="row justify-content-center align-self-center h-100 mt-5">
+        <span class="mt-5" style="width:100%">ENG</span>
+        <span style="width:100%">
+          <img src="../../static/img/search.png" alt>
+        </span>
+        <span style="width:100%">
+          <img src="../../static/img/kafelki_.png" alt>
+        </span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: "Main"
+  name: "Main",
+  data: () => ({
+    mapStyle: {
+     styles: [
+  {
+    "stylers": []
+  },
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "saturation": -30
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#85E3FF"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#FFFFFF"
+      },
+      {
+        "weight": 3
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#FFFFFF"
+      },
+      {
+        "weight": 3
+      }
+    ]
+  }
+]
+    }
+  })
 };
 </script>
 
@@ -24,6 +314,7 @@ export default {
   color: white;
   text-align: center;
   align-items: center;
+  vertical-align: middle;
   flex: 1;
 }
 </style>
